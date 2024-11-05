8722beaf1d69e8eb790210288e727a6305c8a3c7
vishnu <vishnu012@protonmail.com>
2024-11-05 18:46:06 +0530

    initial commit for STM8S103 development

--- include/stm8s.h
+++ include/stm8s.h
@@ -45,7 +45,7 @@
  /* #define STM8S005 */      /*!< STM8S Value Line Medium density devices */
  /* #define STM8AF626x */    /*!< STM8A Medium density devices */
  /* #define STM8AF622x */    /*!< STM8A Low density devices */
- /* #define STM8S103 */      /*!< STM8S Low density devices */
+#define STM8S103             /*!< STM8S Low density devices */
  /* #define STM8S003 */      /*!< STM8S Value Line Low density devices */
  /* #define STM8S903 */      /*!< STM8S Low density devices */
  /* #define STM8S001 */      /*!< STM8S Value Line Low denisty devices */
